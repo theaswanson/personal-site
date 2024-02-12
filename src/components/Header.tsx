1fa8034fc3d3372936f9f7133860c7182ccbed65
Adam Swanson <adam.swanson.98@gmail.com>
2024-02-12 15:30:27 -0500

    Rearrange social icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,37 @@
-import "./Header.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SocialItem } from "../models";
+import "./Header.scss";
 import { faTwitterX } from "./faTwitterX";
 
 function Header() {
   const socialItems: SocialItem[] = [
-    { icon: faTwitterX, href: "https://x.com/crisp2020" },
     { icon: faLinkedin, href: "https://www.linkedin.com/in/adam-swanson/" },
     { icon: faGithub, href: "https://github.com/theaswanson" },
+    { icon: faTwitterX, href: "https://x.com/crisp2020" },
     { icon: faPenSquare, href: "https://blog.adamswanson.dev" },
   ];
 
   return (
-    <div className="header-card">
-      <div className="card-content">
-        <div className="info">
-          <div className="header">
-            <div className="title">Adam Swanson</div>
-            <div className="subtitle">Software Developer</div>
+    <div className='header-card'>
+      <div className='card-content'>
+        <div className='info'>
+          <div className='header'>
+            <div className='title'>Adam Swanson</div>
+            <div className='subtitle'>Software Developer</div>
           </div>
-          <div className="icons">
+          <div className='icons'>
             {socialItems.map((i) => (
-              <div className="icon" key={i.href}>
-                <a href={i.href} target="_blank">
+              <div className='icon' key={i.href}>
+                <a href={i.href} target='_blank'>
                   <FontAwesomeIcon icon={i.icon!} size={"lg"} />
                 </a>
               </div>
             ))}
           </div>
         </div>
-        <div className="image">
+        <div className='image'>
           <img src={"/img/adam.jpg"} />
         </div>
       </div>
