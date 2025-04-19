fd11a0375d5124ee8b1208172d89eea7a1ff1d1c
Adam Swanson <adam.swanson.98@gmail.com>
2025-04-19 15:29:35 -0400

    Rework milestones and skills to use flex and gap instead of padding

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,28 +1,28 @@
-import { Milestone } from "../models";
 import experienceJson from "../data/experience.json";
+import { Milestone } from "../models";
 
 function Experience() {
   const experience = experienceJson as Milestone[];
 
   return (
-    <div className="card" id="experience">
-      <div className="card-content">
-        <div className="info">
-          <div className="header">
-            <div className="title">Experience</div>
+    <div className='card' id='experience'>
+      <div className='card-content'>
+        <div className='info'>
+          <div className='header'>
+            <div className='title'>Experience</div>
           </div>
         </div>
-        <div className="milestones">
+        <div className='milestones'>
           {experience.map((e) => (
-            <div className="milestone" key={e.title! + e.date}>
-              <div className="image">
+            <div className='milestone' key={e.title! + e.date}>
+              <div className='image'>
                 <img src={"/img/" + e.image} />
               </div>
-              <div className="info">
-                <div className="title">{e.title}</div>
-                <div className="subtitle">{e.subtitle}</div>
+              <div className='info'>
+                <div className='title'>{e.title}</div>
+                <div className='subtitle'>{e.subtitle}</div>
               </div>
-              <div className="date">{e.date}</div>
+              <div className='date'>{e.date}</div>
             </div>
           ))}
         </div>
